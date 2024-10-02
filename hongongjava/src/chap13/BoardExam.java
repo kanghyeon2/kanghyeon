6f929f34ddb37ed16777fa952274377943499084
admin <admin@DESKTOP-5FFM27G>
2024-10-02 17:54:19 +0900

    20241002-2

--- hongongjava/src/chap13/BoardExam.java
+++ hongongjava/src/chap13/BoardExam.java
@@ -7,6 +7,7 @@ public class BoardExam {
 		BoardDao dao = new BoardDao();
 		List<Board> list = dao.getBoardList();
 		for(Board board : list) {
+			System.out.println(board);
 			System.out.println(board.getTitle() + "-" + board.getContent()+ "-" + board.getWriter());
 		}		
 	}
